36e22a5bdd424cce708601b8a109966127ecd78a
Ítalo Henrique <italodssouza@gmail.com>
2019-10-24 08:47:49 +0100

    First Commit with Template

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Alert } from "shards-react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+function App() {
+  return (
+    <div className="example">
+      <Alert theme="primary">
+        Alert - Primary Theme (default) -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="secondary">
+        Alert - Secondary Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="success">
+        Alert - Success Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="danger">
+        Alert - Danger Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="warning">
+        Alert - Warning Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="info">
+        Alert - Info Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="light">
+        Alert - Light Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+      <Alert theme="dark">
+        Alert - Dark Theme -{" "}
+        <a className="alert-link" href="#">
+          Example Link
+        </a>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
